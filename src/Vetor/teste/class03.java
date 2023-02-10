a078a7f10dfc811aca92b1240e5c2931e69e4938
Augusto <joao.augusto62@aluno.ifce.edu.br>
2023-02-10 11:58:05 -0300

    feat: add toString method

--- src/Vetor/teste/class03.java
+++ src/Vetor/teste/class03.java
@@ -6,14 +6,10 @@ public class class03 {
     public static void main(String[] args) {
         Vetor vetor = new Vetor(5);
 
-        try {
-            vetor.add("Augusto");
-            vetor.add("João");
-            vetor.add("João");
-            vetor.add("João");
-            vetor.add("João");
-        } catch(Exception e) {
-            e.printStackTrace();
-        }
+        vetor.add("Augusto");
+        vetor.add("João");
+        vetor.add("João");
+        vetor.add("João");
+        vetor.add("João");
     }
 }
---
+++ src/Vetor/teste/class04.java
@@ -0,0 +1,16 @@
+package Vetor.teste;
+
+import Vetor.Vetor;
+
+public class class04 {
+    public static void main(String[] args) {
+        Vetor vetor = new Vetor(10);
+
+        vetor.add("Augusto");
+        vetor.add("Augusto");
+        vetor.add("Augusto");
+
+        System.out.println(vetor.tamanho());
+        System.out.println(vetor);
+    }
+}
